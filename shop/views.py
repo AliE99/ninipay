ab01c117800c15ee11e716d47fe9f3adba47b7c2
AliE99 <alielahi.1378@gmail.com>
2022-05-08 15:08:04 +0430

    Feat: brand action for accessories

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from .models import Food, Cloths, Accessory
 from .serializers import FoodSerializer, ClothsSerializer, AccessorySerializer
 
@@ -19,3 +21,11 @@ class ClothsViewSet(ModelViewSet):
 class AccessoryViewSet(ModelViewSet):
     queryset = Accessory.objects.all()
     serializer_class = AccessorySerializer
+
+    @action(detail=False, methods=['GET'])
+    def brand(self, request):
+        brand = request.query_params['search']
+        queryset = Accessory.objects.filter(brand=brand).all()
+        print(queryset)
+        serializer = AccessorySerializer(queryset, many=True)
+        return HttpResponse(serializer.data)
